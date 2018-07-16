443f258c88e47959d192eb2f83613f05475af989
karolinneczka <okaa93@gmail.com>
2018-07-16 11:30:56 +0200

    FInish the task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '813945913163-d1gpsvoqcmt2pdabtmetio34juie1r32.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'ktcg-78yYTem_T4PsdB-zS14',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
